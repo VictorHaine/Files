43c46c9dbb903cd1b72f8f9466edc86722f934fd
Victor Haine <victor.haine@indevs.com.br>
2019-04-23 14:15:46 -0300

    Update pipeline.js

--- js/indevs/pipeline.js
+++ js/indevs/pipeline.js
@@ -14,7 +14,7 @@ const baseWidth = 2;
 const rangeWidth = 4;
 const baseHue = 180;
 const rangeHue = 60;
-const backgroundColor = 'hsla(360, 100%, 100%, 1)';
+const backgroundColor = 'hsla(209, 10%, 79%, 1)';
 
 let container;
 let canvas;
